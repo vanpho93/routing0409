f79b70ff75b273c7add50b3350fe3178a3a8c447
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-01 18:27:04 +0700

    success

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,27 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { SignInService } from './sign-in.service';
 
 @Component({
   selector: 'app-my-reactive-form',
   templateUrl: './sign-in.component.html',
+  providers: [SignInService]
 })
 export class SignInComponent implements OnInit {
   formSignIn: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private signInService: SignInService
+  ) {}
 
   ngOnInit() {
     this.formSignIn = this.fb.group({
-      email: this.fb.control('abcd', Validators.required),
+      username: this.fb.control('abcd', Validators.required),
       password: ''
     });
   }
 
   get txtEmail() {
-    return this.formSignIn.controls.email;
+    return this.formSignIn.controls.username;
   }
 
   onSubmit() {
-    console.log(this.formSignIn.value);
+    const { value } = this.formSignIn;
+    this.signInService.sendSignInRequest(value)
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
   }
 }
