3c56e05d430de4213736ccc051aca05d3cc3af10
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-04 18:23:01 +0700

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SignInService } from './sign-in/sign-in.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,19 @@ import { SignInService } from './sign-in/sign-in.service';
   providers: [SignInService]
 })
 export class AppComponent implements OnInit {
-  constructor(private signInService: SignInService) {}
+  constructor(private signInService: SignInService, private router: Router) {}
 
   ngOnInit(): void {
-    this.signInService.checkToken().then();
+    this.signInService.checkToken();
+  }
+
+  get user() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  signOut() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/signin']);
   }
 }
